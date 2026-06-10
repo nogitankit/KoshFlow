328be13f14496bf41ec79fc4606c8372c890836a
Mayo <as3006533@gmail.com>
2026-06-10 18:30:47 +0530

    some file configurations

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google"
-
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
